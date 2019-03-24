4914681ab55f26ebe4e8167d815f3279860632a0
ControlledChaos <greg@ccdzine.com>
2019-03-24 10:06:28 -0800

    Set featured listings slider to auto play.
    Also loaded only on front page.

--- footer.php
+++ footer.php
@@ -33,11 +33,15 @@ $copyright = sprintf(
 <?php wp_footer(); ?>
 <?php
 // Add the sticky menu script.
+if ( is_front_page() ) :
 echo sprintf(
 	'<script>%1s</script>',
 	'jQuery(document).ready(function(){
-		jQuery(".front-featured-properties ul").slick();
+		jQuery(".front-featured-properties ul").slick({
+			autoplay : true,
+			autoplaySpeed : 7500
+		});
 	  });'
-); ?>
+); endif; ?>
 </body>
 </html>
